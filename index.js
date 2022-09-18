f226c7f166c0e014f1b9e029f0f39d3bd631e2ae
Manish Rai <manishrai@Manishs-MacBook-Pro.local>
2022-09-18 19:48:49 +0530

    Add new req

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.get('/dailyResult' , (req, res)=>{
+    res.send("This is the result");
+})
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("ur app is listening at 3000");
 })
